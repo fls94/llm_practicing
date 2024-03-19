9e90db5a3314c9ed9bfe9d850dbe4d65f0cc96c9
fls94 <39183178+fls94@users.noreply.github.com>
2024-03-19 22:15:28 +0700

    Update models.py

--- models.py
+++ models.py
@@ -6,5 +6,5 @@ class CoreChatModel:
     model_name: str = "gpt-3.5-turbo"
     @classmethod
     def create_model(cls):
-        return ChatOpenAI(api_key= "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", temperature=cls.temperature, model=cls.model_name)
+        return ChatOpenAI(temperature=cls.temperature, model=cls.model_name)
 
